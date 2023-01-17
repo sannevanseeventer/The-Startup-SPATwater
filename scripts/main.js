5a0505a4e6028b36540a8362693b700d59a2d0eb
sannevanseeventer <sanne.van.seeventer@hva.nl>
2023-01-17 17:46:33 +0100

    opbouw mobilefirst

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+const navToggle = document.querySelector("#navToggle")
+const nav = document.querySelector("#nav-links")
+
+navToggle.addEventListener("click", () => {
+    nav.classList.toggle('nav-open')
+})
+
+const mixer = mixitup('.projects__container', {
+    selectors: {
+        target: '.projects__content'
+    },
+    animation: {
+        duration: 600
+    }
+});
+
+const linkProjects = document.querySelectorAll('.projects__item')
+
+function activeProjects(){
+    if(linkProjects){
+        linkProjects.forEach(l=> l.classList.remove('active-projects'))
+        this.classList.add('active-projects')
+    }
+}
+linkProjects.forEach(l=> l.addEventListener('click', activeProjects))
