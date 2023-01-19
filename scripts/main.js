15410d1ae0c0a2a8c598be15717aecc75fcece3c
sannevanseeventer <sanne.van.seeventer@hva.nl>
2023-01-19 11:07:08 +0100

    Teampage toegevoegd

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
+
 const navToggle = document.querySelector("#navToggle")
 const nav = document.querySelector("#nav-links")
-
+ 
 navToggle.addEventListener("click", () => {
     nav.classList.toggle('nav-open')
 })
@@ -24,4 +25,16 @@ function activeProjects(){
 }
 linkProjects.forEach(l=> l.addEventListener('click', activeProjects))
 
+
+let swiper = new Swiper(".discover__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 32,
+    coverflowEffect: {
+        rotate: 0,
+    },
+})
 
